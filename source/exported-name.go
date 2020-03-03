e069c59c893f3699000cdce0f61e19b6cccc00f8
henrytine <tianhangyu8886@gmail.com>
2020-03-03 20:09:44 +0800

    Create exported-name.go

---
+++ source/exported-name.go
@@ -0,0 +1,18 @@
+/**
+ * @Author: henry
+ * @Description:
+ * @File:  exported-name.go
+ * @Version: 1.0.0
+ * @Date: 2020/3/3 8:04 PM
+ */
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+}
